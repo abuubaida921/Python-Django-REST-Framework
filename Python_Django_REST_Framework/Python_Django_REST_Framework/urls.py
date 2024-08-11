b298ea115dc9e1dfaca727dfae2e05fe88f52486
Abu Ubaida <ubaidanazia@gmail.com>
2024-08-11 19:27:38 +0600

    Serializer adding and Viewing all Objects as json in url

--- Python_Django_REST_Framework/Python_Django_REST_Framework/urls.py
+++ Python_Django_REST_Framework/Python_Django_REST_Framework/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('course-list/', views.courses_info),
 ]
--- Python_Django_REST_Framework/rest_api/views.py
+++ Python_Django_REST_Framework/rest_api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from . models import Courses
+from . serializers import CoursesSerializer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse
 
 # Create your views here.
+#QuerySet
+def courses_info(request):
+    #Complex data
+    courses=Courses.objects.all()
+    #Python Dictionary
+    serialized = CoursesSerializer(courses,many=True)
+    #Render to json
+    json_data=JSONRenderer().render(serialized.data)
+    #Send to user for view
+    return HttpResponse(json_data, content_type='application/json')
