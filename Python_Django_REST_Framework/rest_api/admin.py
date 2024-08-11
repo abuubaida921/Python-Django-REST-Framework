7222537bef8558aee2787f95a1524ec45765e90b
Abu Ubaida <ubaidanazia@gmail.com>
2024-08-11 07:11:34 +0600

    Courses model migration creation and data adding

--- Python_Django_REST_Framework/rest_api/admin.py
+++ Python_Django_REST_Framework/rest_api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . models import Courses
 
 # Register your models here.
+@admin.register(Courses)
+
+#display style
+class CoursesAdmin(admin.ModelAdmin):
+    list_display=['id','teacher_name','course_name','course_duration','seat']
