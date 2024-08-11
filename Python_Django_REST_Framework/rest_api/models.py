0542f8aa49aa6c70084983403f654e0594ee5cd4
Abu Ubaida <ubaidanazia@gmail.com>
2024-08-11 07:02:22 +0600

    Courses model and migration creation

--- Python_Django_REST_Framework/rest_api/models.py
+++ Python_Django_REST_Framework/rest_api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Courses(models.Model):
+    teacher_name=models.CharField(max_length=25)
+    course_name=models.CharField(max_length=20)
+    course_duration = models.IntegerField()
+    seat = models.IntegerField()
